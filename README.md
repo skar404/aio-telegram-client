8c7b70a86358c09ed7629f64e8e15764ecf04e00
Denis Malin <denis@malina.page>
2022-08-25 00:08:11 +0400

    fix LICENSE and refactoring

---
+++ README.md
@@ -0,0 +1,4 @@
+# aiohttp telegram client
+
+Only http client for telegram bot.
+use with my telegram bot SDK 
---
+++ aio_telegram_client/client.py
@@ -0,0 +1,44 @@
+from aio_clients import (
+    HttpClient,
+    Options,
+    multipart,
+)
+from aiohttp import ClientTimeout
+
+
+class Client(HttpClient):
+    def __init__(self, token: str):
+        self.token = token
+        super().__init__(
+            host=f'https://api.telegram.org/bot{self.token}/',
+            timeout=10,
+        )
+
+    async def get_updates(self, offset=None, timeout=None):
+        return await self.post('getUpdates', json={
+            'offset': offset,
+            'timeout': timeout,
+        }, o=Options(timeout=ClientTimeout(timeout + 1)))
+
+    async def send_audio(self, chat_id: int, audio: bytes):
+        with multipart.Easy('form-data') as form:
+            form.add_form(multipart.Form(key='chat_id', value=str(chat_id)))
+            form.add_form(multipart.File(key='audio', value=audio, file_name='land.mp3'))
+
+        return await self.post(
+            'sendAudio',
+            data=form,
+            headers=form.headers,
+            o=Options(is_json=True, is_raw=True),
+        )
+
+    async def send_message(self, chat_id: int, text: str):
+        return await self.post('sendMessage', json={
+            'chat_id': chat_id,
+            'text': text,
+        })
+
+    async def get_chat_admins(self, chat_id: int):
+        return await self.post('getChatAdministrators', json={
+            'chat_id': chat_id,
+        })
